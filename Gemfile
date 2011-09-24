b0134bd97a04aaa07a7a364e1be7a4b42e2edd94
Jade Meskill <dev+jade@integrumtech.com>
2011-09-23 19:09:38 -0700

    Added rails project files

---
+++ Gemfile
@@ -0,0 +1,19 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.1.0'
+
+gem 'mysql'
+
+group :assets do
+  gem 'sass-rails', "  ~> 3.1.0"
+  gem 'coffee-rails', "~> 3.1.0"
+  gem 'uglifier'
+end
+
+gem 'jquery-rails'
+
+group :test, :development do
+  gem 'rspec'
+  gem 'ruby-debug19', :require => 'ruby-debug'
+  gem 'capistrano'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RainManServer::Application.config.secret_token = 'a2a65be0672e503bd223dff7b256026b69b7415707b0ebe4c4e5223227b1599dc27fb365f70abfc64810f011a7387bc10e02c66418e8809940ec1c5d5856efef'
